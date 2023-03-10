63fdab941468186291c8738bdf856aa229480833
Wesley Chou <wesleychou9851@gmail.com>
2023-03-09 21:10:52 -0800

    initial commit

---
+++ used-car-price-prediction.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model, preprocessing
+from scipy.stats import norm
+from myutils import remove_nonnumeric
+
