b23d2b6a24403a32633e14bb0a4462aee78fcc3e
Wesley Chou <wesleychou9851@gmail.com>
2023-03-10 20:29:03 -0800

    2nd initial commit

--- used-car-price-prediction.py
+++ used-car-price-prediction.py
@@ -5,3 +5,27 @@ from sklearn import linear_model, preprocessing
 from scipy.stats import norm
 from myutils import remove_nonnumeric
 
+# URL for database
+url = "data/cars-dataset1.csv"
+
+def predict_price(make, model, trim, year, mileage):
+    
+    
+    
+    return None
+
+
+
+def main():
+    
+    make, model, trim = None, None, None  # these should be strings
+    year, mileage = None, None            # these should be integers
+    
+    # get input
+    
+    # output predicted price
+    price = predict_price(make, model, trim, year, mileage)
+    print(f"Predicted price: ${price}")
+
+if __name__ == "__main__":
+    main()
